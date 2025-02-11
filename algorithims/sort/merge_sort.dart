6cf47de536e74d935708c477a5b89c464b5a28c4
ahmedelmaghraby1 <ahmedelmaghraby2014@gmail.com>
2025-02-11 11:06:50 +0200

    Merge Sort

--- algorithims/sort/merge_sort.dart
+++ algorithims/sort/merge_sort.dart
@@ -1,46 +1,50 @@
-void mergeSort(List<int> arr, int left, int right) {
-  if (left < right) {
-    int mid = (left + right) ~/ 2;
+import 'sort.dart';
 
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
-
-    merge(arr, left, mid, right);
+class MergeSort extends Sort {
+  MergeSort({required super.values});
+  @override
+  void sort() {
+    mergeSort(0, values.length - 1);
   }
-}
 
-void merge(List<int> arr, int left, int mid, int right) {
-  List<int> leftArr = arr.sublist(left, mid + 1);
-  List<int> rightArr = arr.sublist(mid + 1, right + 1);
+  void mergeSort(int left, int right) {
+    if (left < right) {
+      int mid = (left + right) ~/ 2;
 
-  int i = 0, j = 0, k = left;
+      mergeSort(left, mid);
+      mergeSort(mid + 1, right);
 
-  while (i < leftArr.length && j < rightArr.length) {
-    if (leftArr[i] <= rightArr[j]) {
+      merge(values, left, mid, right);
+    }
+  }
+
+  void merge(List<int> arr, int left, int mid, int right) {
+    List<int> leftArr = arr.sublist(left, mid + 1);
+    List<int> rightArr = arr.sublist(mid + 1, right + 1);
+
+    int i = 0, j = 0, k = left;
+
+    while (i < leftArr.length && j < rightArr.length) {
+      if (leftArr[i] <= rightArr[j]) {
+        arr[k] = leftArr[i];
+        i++;
+      } else {
+        arr[k] = rightArr[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < leftArr.length) {
       arr[k] = leftArr[i];
       i++;
-    } else {
+      k++;
+    }
+
+    while (j < rightArr.length) {
       arr[k] = rightArr[j];
       j++;
+      k++;
     }
-    k++;
   }
-
-  while (i < leftArr.length) {
-    arr[k] = leftArr[i];
-    i++;
-    k++;
-  }
-
-  while (j < rightArr.length) {
-    arr[k] = rightArr[j];
-    j++;
-    k++;
-  }
-}
-
-void main() {
-  List<int> numbers = [38, 27, 43, 3, 9, 82, 10];
-  mergeSort(numbers, 0, numbers.length - 1);
-  print(numbers); // [3, 9, 10, 27, 38, 43, 82]
 }
--- main.dart
+++ main.dart
@@ -1,9 +1,9 @@
-import 'algorithims/sort/bubble_sort.dart';
+import 'algorithims/sort/merge_sort.dart';
 // import 'algorithims/sort/selection_sort.dart';
 
 void main() {
   List<int> numbers = [14, 3, 7, 23, 25, 26, 11, 92, 54, 94, 97, 102, 105];
-  BubbleSort selectionSort = BubbleSort(values: numbers);
-  selectionSort.sort();
-  selectionSort.display();
+  MergeSort mergeSort = MergeSort(values: numbers);
+  mergeSort.sort();
+  mergeSort.display();
 }
